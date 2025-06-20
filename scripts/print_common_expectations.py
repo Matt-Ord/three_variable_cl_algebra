366f2dc04b14984b86020b6d06855b3614054fb6
Matt Ord <matthew.ord1@gmail.com>
2025-06-20 14:10:22 +0000

    Add state drift script

--- scripts/print_common_expectations.py
+++ scripts/print_common_expectations.py
@@ -14,45 +14,45 @@ from three_variable.equilibrium_squeeze import squeeze_ratio_from_zeta_expr
 from three_variable.symbols import a_dagger_expr, a_expr, k_0, k_minus, k_plus
 
 print("Expect X:")
-sp.print_latex(sp.simplify(expect_x))
+sp.print_latex(sp.simplify(expect_x))  # type: ignore unknown
 print()
 print("Expect X^2:")
-sp.print_latex(expect_x_squared)
+sp.print_latex(expect_x_squared)  # type: ignore unknown
 print()
-sp.print_latex(squeeze_ratio_from_zeta_expr(expect_x_squared))
+sp.print_latex(squeeze_ratio_from_zeta_expr(expect_x_squared))  # type: ignore unknown
 print()
 
 
 print("Expect P:")
-sp.print_latex(sp.simplify(expect_p))
+sp.print_latex(sp.simplify(expect_p))  # type: ignore unknown
 print()
 print("Expect P^2: ")
-sp.print_latex(expect_p_squared)
+sp.print_latex(expect_p_squared)  # type: ignore unknown
 print()
-sp.print_latex(squeeze_ratio_from_zeta_expr(expect_p_squared))
+sp.print_latex(squeeze_ratio_from_zeta_expr(expect_p_squared))  # type: ignore unknown
 print()
 
 print("Uncertainty:")
-sp.print_latex(uncertainty_squared)
+sp.print_latex(uncertainty_squared)  # type: ignore unknown
 print()
-sp.print_latex(sp.factor(squeeze_ratio_from_zeta_expr(uncertainty_squared)))
+sp.print_latex(sp.factor(squeeze_ratio_from_zeta_expr(uncertainty_squared)))  # type: ignore unknown
 print()
 
 print("Expect a:")
-sp.print_latex(expectation_from_formula(a_expr))
+sp.print_latex(expectation_from_formula(a_expr))  # type: ignore unknown
 print("Expect a^dagger:")
-sp.print_latex(expectation_from_formula(a_dagger_expr))
+sp.print_latex(expectation_from_formula(a_dagger_expr))  # type: ignore unknown
 print()
 
 
 print("Expect K_0:")
-sp.print_latex(expectation_from_formula(k_0))
+sp.print_latex(expectation_from_formula(k_0))  # type: ignore unknown
 print()
 
 print("Expect K_+:")
-sp.print_latex(expectation_from_formula(k_plus))
+sp.print_latex(expectation_from_formula(k_plus))  # type: ignore unknown
 print()
 
 print("Expect K_-:")
-sp.print_latex(expectation_from_formula(k_minus))
+sp.print_latex(expectation_from_formula(k_minus))  # type: ignore unknown
 print()
---
+++ scripts/state_drift.py
@@ -0,0 +1,172 @@
+# The pointer states are eigenstates of the matrix A-mu b, were
+# A is the deterministic and B is the non-deterministic part of the SSE
+# Can we predict how these change with mu? this should give us an
+# idea of how <B> etc change with mu too.
+from __future__ import annotations
+
+from typing import override
+
+import sympy as sp
+from sympy.physics.quantum import Dagger, Ket, Operator
+
+
+class AntiHermitianOperator(Operator):
+    @override
+    def _eval_adjoint(self) -> sp.Expr:
+        return -self
+
+
+L = Operator("L")
+
+simple_a_part = AntiHermitianOperator("A_s")
+
+mu = sp.Symbol("mu")
+d_mu = sp.Symbol(r"\partial \mu")
+perturbed_mu = sp.Add(mu, d_mu)
+
+
+initial_state = Ket(r"n_\lambda^\mu")
+state_perturbation = Ket(r"n'_\lambda^\mu")
+other_state = Ket(r"m_\lambda^\mu")
+perturbed_state = sp.Add(initial_state, sp.Mul(d_mu, state_perturbation))
+
+initial_lambda = sp.Symbol(r"\lambda^\mu_n")
+other_lambda = sp.Symbol(r"\lambda^\mu_m")
+
+
+def _apply_drift_part(state: Ket) -> sp.Expr:
+    return L * state * sp.Mul(Dagger(state), Dagger(L), state) - 0.5 * (  # type: ignore unknown
+        (
+            state  # type: ignore unknown
+            * sp.Mul(Dagger(state), Dagger(L), state)  # type: ignore unknown
+            * sp.Mul(Dagger(state), L, state)  # type: ignore unknown
+        )
+        + Dagger(L) * L * state  # type: ignore unknown
+    )
+
+
+def apply_a(state: Ket) -> sp.Expr:
+    return sp.Mul(simple_a_part, state) + _apply_drift_part(state)  # type: ignore unknown
+
+
+def apply_b(state: sp.Expr) -> sp.Expr:
+    return sp.Mul(L, state) - state * sp.Mul(Dagger(state), L, state)  # type: ignore unknown
+
+
+def apply_m(state: sp.Expr, mu: sp.Expr) -> sp.Expr:
+    return apply_a(state) - mu * apply_b(state)  # type: ignore unknown
+
+
+def extract_mu_factor(expr: sp.Expr, n: int) -> sp.Expr:
+    expr = sp.expand(expr)  # type: ignore unknown
+    out: sp.Expr = sp.Number(0)
+    for i in range(n + 1):
+        factor = d_mu**i * Dagger(d_mu) ** (n - i)  # type: ignore unknown
+        try:
+            out += sp.Mul(  # type: ignore unknown
+                sp.collect(expr, [factor], exact=True, evaluate=False)[factor].subs(  # type: ignore unknown
+                    {d_mu: 0, Dagger(d_mu): 0}
+                ),
+                factor,  # type: ignore unknown
+            )
+        except KeyError:
+            continue
+    return out  # type: ignore unknown
+
+
+def simplify_inner_products(expr: sp.Expr) -> sp.Expr:
+    return sp.expand(expr).subs(  # type: ignore unknown
+        {  # States are normalized
+            sp.Mul(Dagger(initial_state), initial_state): 1,  # type: ignore unknown
+            sp.Mul(Dagger(state_perturbation), state_perturbation): 1,  # type: ignore unknown
+            sp.Mul(Dagger(other_state), other_state): 1,  # type: ignore unknown
+            # The initial state is orthogonal to the perturbation
+            sp.Mul(Dagger(initial_state), state_perturbation): 0,  # type: ignore unknown
+            sp.Mul(Dagger(state_perturbation), initial_state): 0,  # type: ignore unknown
+            # The other state is orthogonal to the initial
+            sp.Mul(Dagger(other_state), initial_state): 0,  # type: ignore unknown
+            sp.Mul(Dagger(initial_state), other_state): 0,  # type: ignore unknown
+        }
+    )
+
+
+def _get_operator_expansion(operator: Operator) -> sp.Expr:
+    out: sp.Expr = sp.Number(0)
+    for i, state_i in enumerate([initial_state, state_perturbation]):
+        for j, state_j in enumerate([initial_state, state_perturbation]):
+            out += sp.Mul(  # type: ignore unknown
+                sp.Symbol(f"{operator.label[0]}_{i}{j}"),  # type: ignore unknown
+                state_i,
+                Dagger(state_j),  # type: ignore unknown
+            )
+    return out  # type: ignore unknown
+
+
+def express_as_c_number(expr: sp.Expr, operator: Operator) -> sp.Expr:
+    expansion = _get_operator_expansion(operator)
+    return sp.simplify(expr.subs({operator: expansion}))  # type: ignore unknown
+
+
+def _get_operator_c_numbers(operator: Operator) -> dict[sp.Symbol, sp.Expr]:
+    out = dict[sp.Symbol, sp.Expr]()
+    for i, state_i in enumerate([initial_state, state_perturbation]):
+        for j, state_j in enumerate([initial_state, state_perturbation]):
+            symbol = sp.Symbol(f"{operator.label[0]}_{i}{j}")  # type: ignore unknown
+            out[sp.conjugate(symbol)] = Dagger(state_j) * Dagger(operator) * state_i  # type: ignore unknown
+            out[symbol] = Dagger(state_i) * operator * state_j  # type: ignore unknown
+    return out
+
+
+def express_as_inner_products(expr: sp.Expr, operator: Operator) -> sp.Expr:
+    c_numbers = _get_operator_c_numbers(operator)
+    return expr.subs(c_numbers)  # type: ignore unknown
+
+
+def get_first_order_perturbation_initial_state() -> sp.Expr:
+    # Evaluate the functional for the perturbed state
+    functional_perturbed_first_order = extract_mu_factor(
+        apply_m(perturbed_state, perturbed_mu),
+        1,
+    )
+    # Find the component in the direction of the initial state
+    out = sp.expand(sp.Mul(Dagger(initial_state), functional_perturbed_first_order))  # type: ignore unknown
+    # Use initial eigenvalue equation to replace the hamiltonian dependent part
+    out = out.subs(  # type: ignore unknown
+        {
+            -Dagger(simple_a_part * initial_state): -Dagger(  # type: ignore unknown
+                initial_lambda * initial_state + _apply_drift_part(initial_state)  # type: ignore unknown
+            )
+        }
+    )
+    # Replace all inner products with the initial state
+    out = simplify_inner_products(out)  # type: ignore unknown
+    # Replace all inner products with their c numbers and simplify
+    return express_as_inner_products(
+        simplify_inner_products(express_as_c_number(out, L)), L
+    )
+
+
+def get_first_order_perturbation_other_state() -> sp.Expr:
+    # Evaluate the functional for the perturbed state
+    functional_perturbed_first_order = extract_mu_factor(
+        apply_m(perturbed_state, perturbed_mu), 1
+    )
+    # Find the component in the direction of the initial state
+    out = sp.expand(sp.Mul(Dagger(other_state), functional_perturbed_first_order))  # type: ignore unknown
+    # Use initial eigenvalue equation to replace the hamiltonian dependent part
+    out = out.subs(  # type: ignore unknown
+        {
+            -Dagger(simple_a_part * other_state): -Dagger(  # type: ignore unknown
+                other_lambda * other_state + _apply_drift_part(other_state)  # type: ignore unknown
+            )
+        }
+    )
+    # Replace all inner products with the initial state
+    return simplify_inner_products(out)  # type: ignore unknown
+
+
+print("B first order terms")
+print("Initial state")
+sp.print_latex(get_first_order_perturbation_initial_state())  # type: ignore unknown
+print("Other state")
+sp.print_latex(get_first_order_perturbation_other_state())  # type: ignore unknown
