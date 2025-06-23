7440ed9e6a40a8905ae0b5938adc5293d74c307c
chaizxcam <zc362@cam.ac.uk>
2025-06-23 11:53:58 +0000

    Merge fixes in alpha and script to get equation of motion on a new branch equation-of-motion

--- scripts/central_derivative.py
+++ scripts/central_derivative.py
@@ -2,14 +2,10 @@ from __future__ import annotations
 
 import sympy as sp
 
-from three_variable.equilibrium_squeeze import (
-    squeeze_ratio_from_zeta_expr,
-)
 from three_variable.projected_sse import (
     get_environment_derivative,
     get_system_derivative,
 )
-from three_variable.symbols import alpha
 
 print("The Raw derivaties which describe the alpha parameter:")
 expr_system = sp.simplify(get_system_derivative("alpha")).subs({sp.Symbol("V_1"): 0})
@@ -21,21 +17,19 @@ print()
 expr_environment = get_environment_derivative("alpha")
 expr_environment = sp.simplify(sp.together(sp.expand(expr_environment)))
 print("Environment derivative:")
+# sp.print_latex(expr_environment)
+expr_environment = sp.collect(
+    expr_environment, [sp.Symbol("alpha"), sp.conjugate(sp.Symbol("alpha"))]
+)
 sp.print_latex(expr_environment)
 
-x = sp.Symbol("x", real=True)
-p = sp.Symbol("p", real=True)
+# x = sp.Symbol("x", real=True)
+# p = sp.Symbol("p", real=True)
 
-expr_r_environment = sp.factor(squeeze_ratio_from_zeta_expr(expr_environment))
-# expr_r_environment = expr_r_environment.subs(
-#     {
-#         alpha: (x - p),
-#         sp.conjugate(alpha): (x + p),
-#     }
+# expr_r_environment = sp.factor(squeeze_ratio_from_zeta_expr(expr_environment))
+# expr_r_environment = sp.collect(
+#     sp.factor(sp.expand(expr_r_environment)), [alpha, sp.conjugate(alpha)]
 # )
-expr_r_environment = sp.collect(
-    sp.factor(sp.expand(expr_r_environment)), [alpha, sp.conjugate(alpha)]
-)
-print("Environment derivative (R):")
-sp.print_latex(expr_r_environment)
-print()
+# print("Environment derivative (R):")
+# sp.print_latex(expr_r_environment)
+# print()
