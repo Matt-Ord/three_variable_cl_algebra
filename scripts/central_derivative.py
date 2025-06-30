2be623666a1d7466416405b479b588a28080780f
chaizxcam <zc362@cam.ac.uk>
2025-06-30 12:15:09 +0000

    update equation of motions, include transformation matrices

--- scripts/central_derivative.py
+++ scripts/central_derivative.py
@@ -2,17 +2,30 @@ from __future__ import annotations
 
 import sympy as sp
 
+from three_variable.equilibrium_squeeze import (
+    squeeze_ratio_from_zeta_expr,
+)
 from three_variable.projected_sse import (
     get_environment_derivative,
     get_system_derivative,
 )
+from three_variable.symbols import alpha
 
 print("The Raw derivaties which describe the alpha parameter:")
 expr_system = sp.simplify(get_system_derivative("alpha"))  # type: ignore subs
-expr_system = sp.subs(expr_system, {sp.Symbol("V_1"): 0})  # type: ignore subs
+expr_system = expr_system.subs({sp.Symbol("V_1"): 0})  # type: ignore subs
 expr_system = sp.factor(expr_system)  # type: ignore unknown
 print("System derivative:")
 sp.print_latex(expr_system)  # type: ignore subs
+print()
+
+expr_r_system = sp.factor(squeeze_ratio_from_zeta_expr(expr_system))
+expr_r_system = sp.collect(  # type: ignore collect
+    sp.factor(sp.expand(expr_r_system)),  # type: ignore subs
+    [alpha, sp.conjugate(alpha)],  # type: ignore subs
+)
+print("System derivative (R):")
+sp.print_latex(expr_r_system)  # type: ignore subs
 print()
 
 expr_environment = get_environment_derivative("alpha")
@@ -22,9 +35,6 @@ expr_environment = sp.collect(
     expr_environment, [sp.Symbol("alpha"), sp.conjugate(sp.Symbol("alpha"))]
 )
 sp.print_latex(expr_environment)
-
-# x = sp.Symbol("x", real=True)
-# p = sp.Symbol("p", real=True)
 
 expr_r_environment = sp.factor(squeeze_ratio_from_zeta_expr(expr_environment))  # type: ignore subs
 expr_r_environment = sp.collect(  # type: ignore collect
