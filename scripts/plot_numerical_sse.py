98e7ebc682e68af6e713e2229b4bf74342b19bdf
chaizxcam <zc362@cam.ac.uk>
2025-08-01 09:19:24 +0000

    update gitignore
    simulating sse
    
    redefine squeeze ratio
    
    Delete scripts/energy_distribution.py
    
    Fix lints

--- scripts/plot_numerical_sse.py
+++ scripts/plot_numerical_sse.py
@@ -8,8 +8,11 @@ from matplotlib.ticker import MaxNLocator
 
 from three_variable.simulation import (
     EtaParameters,
+    KBT_value,
     SimulationConfig,
     SimulationResult,
+    evaluate_equilibrium_squeeze_ratio,
+    hbar_value,
     run_projected_simulation,
 )
 
@@ -18,7 +21,7 @@ if TYPE_CHECKING:
     from matplotlib.figure import Figure
 
 
-def plot_alpha_zeta_evolution(
+def plot_alpha_r_evolution(
     result: SimulationResult,
 ) -> tuple[Figure, tuple[Axes, Axes]]:
     fig, [ax0, ax1] = plt.subplots(nrows=2)
@@ -31,15 +34,15 @@ def plot_alpha_zeta_evolution(
     ax0.set_title("Alpha Evolution")
     ax0.set_xlabel("Time")
     ax0.set_ylabel("Alpha")
-    ax1.legend(handles=[line0, line1], labels=["$\\Re(\\alpha)$", "$\\Im(\\alpha)$"])
+    ax0.legend(handles=[line0, line1], labels=["$\\Re(\\alpha)$", "$\\Im(\\alpha)$"])
 
-    (line0,) = ax1.plot(result.times, result.zeta.real)
-    (line1,) = ax1.twinx().plot(result.times, result.zeta.imag)
+    (line0,) = ax1.plot(result.times, result.squeeze_ratio.real)
+    (line1,) = ax1.twinx().plot(result.times, result.squeeze_ratio.imag)
     line1.set_color("C1")
-    ax1.set_title("Zeta Evolution")
+    ax1.set_title("r0 Evolution")
     ax1.set_xlabel("Time")
-    ax1.set_ylabel("Zeta")
-    ax1.legend(handles=[line0, line1], labels=["$\\Re(\\zeta)$", "$\\Im(\\zeta)$"])
+    ax1.set_ylabel("r0")
+    ax1.legend(handles=[line0, line1], labels=["$\\Re(R0)$", "$\\Im(R0)$"])
 
     fig.tight_layout()
     return fig, (ax0, ax1)
@@ -51,8 +54,8 @@ def plot_classical_eom_comparison(
     fig, [ax0, ax1] = plt.subplots(nrows=2)
     ax0 = cast("Axes", ax0)
     ax1 = cast("Axes", ax1)
-
-    ax0.plot(result.times, result.x, label="$x$")
+    stiffness = -0.5 * result.params.eta_m * KBT_value / (result.params.eta_omega**2)
+    ax0.plot(result.times, stiffness * result.x, label=r"$-x m \omega^2$")
     dpdt = np.gradient(result.p, result.times)
     ax0.plot(result.times, dpdt, label="$\\frac{dp}{dt}$")
     ax0.set_title("X Evolution")
@@ -62,7 +65,37 @@ def plot_classical_eom_comparison(
 
     ax1.plot(result.times, result.p, label="$p$")
     dxdt = np.gradient(result.x, result.times)
-    ax1.plot(result.times, (dxdt / 2) / result.params.eta_m, label="$\\frac{dx}{dt}$")
+    m = result.params.eta_m * hbar_value**2 / (2 * KBT_value)
+    ax1.plot(result.times, dxdt * m, label="$\\frac{dx}{dt} m$")
+    ax1.set_title("P Evolution")
+    ax1.set_xlabel("Time")
+    ax1.set_ylabel("P")
+    ax1.legend()
+
+    fig.tight_layout()
+    return fig, (ax0, ax1)
+
+
+def plot_classical_evolution_formula(
+    result: SimulationResult,
+) -> tuple[Figure, tuple[Axes, Axes]]:
+    fig, [ax0, ax1] = plt.subplots(nrows=2)
+    ax0 = cast("Axes", ax0)
+    ax1 = cast("Axes", ax1)
+    stiffness = -0.5 * result.params.eta_m * KBT_value / (result.params.eta_omega**2)
+    ax0.plot(result.times, stiffness * result.x, label="$x$")
+    dpdt = np.gradient(result.p, result.times)
+    ax0.plot(result.times, dpdt, label="$\\frac{dp}{dt}$")
+    ax0.set_title("X Evolution")
+    ax0.set_xlabel("Time")
+    ax0.set_ylabel("X")
+    ax0.legend()
+
+    ax1.plot(result.times, result.p, label="$p$")
+    m = result.params.eta_m * hbar_value**2 / (2 * KBT_value)
+    ax1.plot(
+        result.times, result.x_derivative_equilibrium * m, label="$\\frac{dx}{dt} m$"
+    )
     ax1.set_title("P Evolution")
     ax1.set_xlabel("Time")
     ax1.set_ylabel("P")
@@ -129,33 +162,40 @@ def plot_r_theta_evolution(
 
 
 if __name__ == "__main__":
-    eta_m_val = 1
-
+    eta_m_val = 1e22
+    eta_lamda_val = 60
+    eta_omega_val = 1
+    time_scale = hbar_value / KBT_value
+    print("Estimating initial r0")
+    equilibrium_ratio = evaluate_equilibrium_squeeze_ratio(
+        eta_lambda_val=eta_lamda_val,
+        eta_omega_val=eta_omega_val,
+    )
     print("Running simulation")
     solution = run_projected_simulation(
         SimulationConfig(
             params=EtaParameters(
-                eta_lambda=0.01,
+                eta_lambda=eta_lamda_val,
                 eta_m=eta_m_val,
-                eta_omega=1,
-                kbt_div_hbar=1.0,
+                eta_omega=eta_omega_val,
+                kbt_div_hbar=1,
             ),
-            alpha_0=0.0 + 0.0j,
-            zeta_0=0.003 * np.exp(1j * 0.01),
-            times=np.linspace(0, 5, int(eta_m_val * 1000)),
+            alpha_0=0.000001 + 0.0j,
+            r_0=equilibrium_ratio,
+            times=np.linspace(0, 1000, 50000) * time_scale,
         )
     )
 
     print("Simulation completed")
-    print("Final (equilibrium) zeta:", solution.zeta[-1])
+    print("Final (equilibrium) r:", solution.squeeze_ratio[-1])
 
-    fig, _ = plot_alpha_zeta_evolution(solution)
-    fig.savefig("alpha_zeta_evolution.png", dpi=300)
+    fig, _ = plot_alpha_r_evolution(solution)
+    fig.savefig("alpha_r_evolution.png", dpi=300)
 
-    fig, _ = plot_classical_eom_comparison(solution[-300::])
+    fig, _ = plot_classical_eom_comparison(solution[-1000::])
     fig.savefig("classical_eom_comparison.png", dpi=300)
 
-    fig, _ = plot_classical_evolution(solution[-300::])
+    fig, _ = plot_classical_evolution(solution[-1000::])
     fig.savefig("classical_evolution.png", dpi=300)
 
     solution = run_projected_simulation(
@@ -167,7 +207,7 @@ if __name__ == "__main__":
                 kbt_div_hbar=1.0,
             ),
             alpha_0=0.0 + 0.0j,
-            zeta_0=0.005 * np.exp(1j * 0.1),
+            r_0=1.5 * equilibrium_ratio * np.exp(1j * 0.1),
             times=np.linspace(0, 0.03, int(eta_m_val * 1000)),
         )
     )
