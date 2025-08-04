2042ca35d81d344ea687b46ec7a5d3b620006e0d
chaizxcam <zc362@cam.ac.uk>
2025-08-04 14:48:00 +0000

    simulating sse

--- scripts/plot_numerical_sse.py
+++ scripts/plot_numerical_sse.py
@@ -7,8 +7,11 @@ import numpy as np
 
 from three_variable.simulation import (
     EtaParameters,
+    KBT_value,
     SimulationConfig,
     SimulationResult,
+    estimate_r0_initial,
+    hbar_value,
     run_projected_simulation,
 )
 
@@ -17,7 +20,7 @@ if TYPE_CHECKING:
     from matplotlib.figure import Figure
 
 
-def plot_alpha_zeta_evolution(
+def plot_alpha_r_evolution(
     result: SimulationResult,
 ) -> tuple[Figure, tuple[Axes, Axes]]:
     fig, [ax0, ax1] = plt.subplots(nrows=2)
@@ -30,15 +33,15 @@ def plot_alpha_zeta_evolution(
     ax0.set_title("Alpha Evolution")
     ax0.set_xlabel("Time")
     ax0.set_ylabel("Alpha")
-    ax1.legend(handles=[line0, line1], labels=["$\\Re(\\alpha)$", "$\\Im(\\alpha)$"])
+    ax0.legend(handles=[line0, line1], labels=["$\\Re(\\alpha)$", "$\\Im(\\alpha)$"])
 
-    (line0,) = ax1.plot(result.times, result.zeta.real)
-    (line1,) = ax1.twinx().plot(result.times, result.zeta.imag)
+    (line0,) = ax1.plot(result.times, result.r0.real)
+    (line1,) = ax1.twinx().plot(result.times, result.r0.imag)
     line1.set_color("C1")
-    ax1.set_title("Zeta Evolution")
+    ax1.set_title("r0 Evolution")
     ax1.set_xlabel("Time")
-    ax1.set_ylabel("Zeta")
-    ax1.legend(handles=[line0, line1], labels=["$\\Re(\\zeta)$", "$\\Im(\\zeta)$"])
+    ax1.set_ylabel("r0")
+    ax1.legend(handles=[line0, line1], labels=["$\\Re(R0)$", "$\\Im(R0)$"])
 
     fig.tight_layout()
     return fig, (ax0, ax1)
@@ -50,8 +53,8 @@ def plot_classical_evolution(
     fig, [ax0, ax1] = plt.subplots(nrows=2)
     ax0 = cast("Axes", ax0)
     ax1 = cast("Axes", ax1)
-
-    ax0.plot(result.times, result.x, label="$x$")
+    stiffness = -0.5 * result.params.eta_m * KBT_value / (result.params.eta_omega**2)
+    ax0.plot(result.times, stiffness * result.x, label=r"$-x m \omega^2$")
     dpdt = np.gradient(result.p, result.times)
     ax0.plot(result.times, dpdt, label="$\\frac{dp}{dt}$")
     ax0.set_title("X Evolution")
@@ -61,7 +64,37 @@ def plot_classical_evolution(
 
     ax1.plot(result.times, result.p, label="$p$")
     dxdt = np.gradient(result.x, result.times)
-    ax1.plot(result.times, (dxdt / 2) / result.params.eta_m, label="$\\frac{dx}{dt}$")
+    m = result.params.eta_m * hbar_value**2 / (2 * KBT_value)
+    ax1.plot(result.times, dxdt * m, label="$\\frac{dx}{dt} m$")
+    ax1.set_title("P Evolution")
+    ax1.set_xlabel("Time")
+    ax1.set_ylabel("P")
+    ax1.legend()
+
+    fig.tight_layout()
+    return fig, (ax0, ax1)
+
+
+def plot_classical_evolution_formula(
+    result: SimulationResult,
+) -> tuple[Figure, tuple[Axes, Axes]]:
+    fig, [ax0, ax1] = plt.subplots(nrows=2)
+    ax0 = cast("Axes", ax0)
+    ax1 = cast("Axes", ax1)
+    stiffness = -0.5 * result.params.eta_m * KBT_value / (result.params.eta_omega**2)
+    ax0.plot(result.times, stiffness * result.x, label="$x$")
+    dpdt = np.gradient(result.p, result.times)
+    ax0.plot(result.times, dpdt, label="$\\frac{dp}{dt}$")
+    ax0.set_title("X Evolution")
+    ax0.set_xlabel("Time")
+    ax0.set_ylabel("X")
+    ax0.legend()
+
+    ax1.plot(result.times, result.p, label="$p$")
+    m = result.params.eta_m * hbar_value**2 / (2 * KBT_value)
+    ax1.plot(
+        result.times, result.x_derivative_equilibrium * m, label="$\\frac{dx}{dt} m$"
+    )
     ax1.set_title("P Evolution")
     ax1.set_xlabel("Time")
     ax1.set_ylabel("P")
@@ -72,30 +105,37 @@ def plot_classical_evolution(
 
 
 if __name__ == "__main__":
-    eta_m_val = 1
-
+    eta_m_val = 1e22
+    eta_lamda_val = 60
+    eta_omega_val = 1
+    time_scale = hbar_value / KBT_value
+    print("Estimating initial r0")
+    r0_initial_estimate = estimate_r0_initial(
+        eta_lambda_val=eta_lamda_val,
+        eta_omega_val=eta_omega_val,
+    )
     print("Running simulation")
     solution = run_projected_simulation(
         SimulationConfig(
             params=EtaParameters(
-                eta_lambda=0.01,
+                eta_lambda=eta_lamda_val,
                 eta_m=eta_m_val,
-                eta_omega=1,
-                kbt_div_hbar=1.0,
+                eta_omega=eta_omega_val,
+                kbt_div_hbar=1,
             ),
-            alpha_0=1.0 + 0.0j,
-            zeta_0=-2 / 3 + 0.0j,
-            times=np.linspace(0, 5, int(eta_m_val * 1000)),
+            alpha_0=0.000001 + 0.0j,
+            r0_initial=r0_initial_estimate,
+            times=np.linspace(0, 1000, 50000) * time_scale,
         )
     )
 
     print("Simulation completed")
-    print("Final (equilibrium) zeta:", solution.zeta[-1])
+    print("Final (equilibrium) r:", solution.r0[-1])
 
-    fig, _ = plot_alpha_zeta_evolution(solution)
-    fig.savefig("alpha_zeta_evolution.png", dpi=300)
+    fig, _ = plot_alpha_r_evolution(solution)
+    fig.savefig("alpha_r_evolution.png", dpi=300)
 
-    fig, _ = plot_classical_evolution(solution[-300::])
+    fig, _ = plot_classical_evolution(solution[-10000::])
     fig.savefig("classical_evolution.png", dpi=300)
 
     plt.show()
