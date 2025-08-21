05a9fcb36adc30eaddfb6f1b6aad43b293f8f76f
Matt Ord <matthew.ord1@gmail.com>
2025-08-21 14:43:59 +0000

    Improved x,p plots

--- scripts/plot_numerical_sse.py
+++ scripts/plot_numerical_sse.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING, cast
 
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.ticker import MaxNLocator
 
 from three_variable.simulation import (
     EtaParameters,
@@ -44,7 +45,7 @@ def plot_alpha_zeta_evolution(
     return fig, (ax0, ax1)
 
 
-def plot_classical_evolution(
+def plot_classical_eom_comparison(
     result: SimulationResult,
 ) -> tuple[Figure, tuple[Axes, Axes]]:
     fig, [ax0, ax1] = plt.subplots(nrows=2)
@@ -71,6 +72,62 @@ def plot_classical_evolution(
     return fig, (ax0, ax1)
 
 
+def plot_classical_evolution(
+    result: SimulationResult,
+) -> tuple[Figure, tuple[Axes, Axes]]:
+    fig, ax0 = plt.subplots(figsize=(5, 3))
+    ax1 = ax0.twinx()
+
+    times = result.times - result.times[0]
+    (line0,) = ax0.plot(times, result.x, label="$x$")
+    line0.set_color("C0")
+
+    (line1,) = ax1.plot(times, result.p, label="$p$")
+    line1.set_color("C1")
+
+    # Offset the two plots, so they don't overlap
+    start, end = ax0.get_ylim()
+    ax0.set_ylim(start - 0.5 * (end - start), end)
+    start, end = ax1.get_ylim()
+    ax1.set_ylim(start, end + 0.5 * (end - start))
+
+    ax0.set_xlabel("Time /au")
+    ax0.set_ylabel("X /au")
+    ax1.set_ylabel("P /au")
+    ax0.set_title("Evolution of X and P")
+    ax1.legend(handles=[line0, line1], labels=["$x$", "$p$"])
+
+    fig.tight_layout()
+    ax1.yaxis.set_major_locator(MaxNLocator(nbins=5))
+    ax0.yaxis.set_major_locator(MaxNLocator(nbins=5))
+    ax1.xaxis.set_major_locator(MaxNLocator(nbins=4))
+    return fig, (ax0, ax1)
+
+
+def plot_r_theta_evolution(
+    result: SimulationResult,
+) -> tuple[Figure, tuple[Axes, Axes]]:
+    fig, ax0 = plt.subplots(figsize=(5, 3))
+
+    (line0,) = ax0.plot(result.times, np.abs(result.zeta))
+    ax1 = ax0.twinx()
+    (line1,) = ax1.plot(result.times, np.unwrap(np.angle(result.zeta)))
+    line1.set_color("C1")
+    ax0.set_title("Squeezing Evolution")
+    ax0.set_xlabel("Time /au")
+    ax0.set_ylabel("Squeezing $r$")
+    ax1.set_ylabel(r"Squeezing $\theta$")
+    ax0.set_ylim(0, None)
+    ax1.set_ylim(0, None)
+    ax1.legend(handles=[line0, line1], labels=["$r$", r"$\theta$"])
+
+    fig.tight_layout()
+    ax1.yaxis.set_major_locator(MaxNLocator(nbins=3))
+    ax0.yaxis.set_major_locator(MaxNLocator(nbins=3))
+    ax0.xaxis.set_major_locator(MaxNLocator(nbins=4))
+    return fig, (ax0, ax1)
+
+
 if __name__ == "__main__":
     eta_m_val = 1
 
@@ -83,8 +140,8 @@ if __name__ == "__main__":
                 eta_omega=1,
                 kbt_div_hbar=1.0,
             ),
-            alpha_0=1.0 + 0.0j,
-            zeta_0=-2 / 3 + 0.0j,
+            alpha_0=0.0 + 0.0j,
+            zeta_0=-0.002 + 0.0j,
             times=np.linspace(0, 5, int(eta_m_val * 1000)),
         )
     )
@@ -95,7 +152,26 @@ if __name__ == "__main__":
     fig, _ = plot_alpha_zeta_evolution(solution)
     fig.savefig("alpha_zeta_evolution.png", dpi=300)
 
+    fig, _ = plot_classical_eom_comparison(solution[-300::])
+    fig.savefig("classical_eom_comparison.png", dpi=300)
+
     fig, _ = plot_classical_evolution(solution[-300::])
     fig.savefig("classical_evolution.png", dpi=300)
+
+    solution = run_projected_simulation(
+        SimulationConfig(
+            params=EtaParameters(
+                eta_lambda=0.01,
+                eta_m=eta_m_val,
+                eta_omega=1,
+                kbt_div_hbar=1.0,
+            ),
+            alpha_0=0.0 + 0.0j,
+            zeta_0=0.005 * np.exp(1j * 0.1),
+            times=np.linspace(0, 0.03, int(eta_m_val * 1000)),
+        )
+    )
+    fig, _ = plot_r_theta_evolution(solution)
+    fig.savefig("r_theta_evolution.png", dpi=600)
 
     plt.show()
